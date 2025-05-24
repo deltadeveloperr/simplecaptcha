6939a010c246767f8b4e3d343948c64ce2a28fce
Delta Atlas Ercan Ares <114815670+deltadeveloperr@users.noreply.github.com>
2025-05-24 18:08:22 +0300

    Create README.lua

---
+++ simplecaptcha/README.lua
@@ -0,0 +1,10 @@
+--[[
+simplecaptcha is just a simple, lightweight, customizable and
+open source Captcha system for games to prevent robots.
+
+Installation:
+1. Move CaptchaManager into ServerScriptService.
+2. Move ChallengeGui into StarterGui.
+3. Move CaptchaCheck into ReplicatedStorage.
+4. Delete this script and folder.
+]]
